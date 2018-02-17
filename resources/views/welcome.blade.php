4e67049a0182619db9d73a6a24d1b0fe841677a7
Nathan Daly <nathandalypretoria@hotmail.co.uk>
2018-02-17 01:03:09 +0000

    Update welcome page to reflect InstandaFlow.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    <img height="200px" width="200px" src="{{ asset('images/big-icon.png') }}"> <br/>
+                    InstandaFlow
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="#">Documentation</a>
+                    <a href="https://github.com/nathandaly/InstandaFlow">GitHub</a>
                 </div>
             </div>
         </div>
