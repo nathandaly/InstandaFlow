fa7c2365f9f521c813efe90c32af79662f2a47da
Nathan Daly <justlikephp@gmail.com>
2018-02-27 21:56:21 +0000

    Added an association between unsubscribers and users.

--- database/migrations/2018_02_09_012711_Unsubscribe.php
+++ database/migrations/2018_02_09_012711_Unsubscribe.php
@@ -15,10 +15,12 @@ class Unsubscribe extends Migration
     {
         Schema::create('unsubscribers', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
             $table->string('email');
             $table->string('integration');
             $table->string('hook');
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
@@ -29,6 +31,6 @@ class Unsubscribe extends Migration
      */
     public function down()
     {
-        Schema::drop('flights');
+        Schema::drop('unsubscribers');
     }
 }
